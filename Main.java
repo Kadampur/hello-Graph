678366fe4a46d7c99b1ac6862baadcfb642546ae
Kadampur <ali.kadampur@gmail.com>
2018-10-11 11:31:48 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		ReadGraph rg1=new ReadGraph();
+		//ComputeClearing cc1=new ComputeClearing();
+		//LinkedList lx=new LinkedList();
+		//LinkedList E=new LinkedList(lx);
+		//ArrayList<LinkedList<String>> lxs=new ArrayList<LinkedList<String>>();
+		
+		//lxs=
+		rg1.readCSVFile();
+		//cc1.findDisjointCycles(lxs, directed);
+	}
+
+}
